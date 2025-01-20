284f9bd0d3822d70a99b8611e08cc6101a5a0cca
ChibuikeOnuba <onubawinner042@gmail.com>
2025-01-20 01:24:59 +0100

    api correction

--- database_utils.py
+++ database_utils.py
@@ -1,12 +1,52 @@
 import streamlit as st
 from typing_extensions import TypedDict
 from pages.connection import main
+import getpass
+import os
+from langchain import hub
+from langchain_openai import ChatOpenAI
 
 class State(TypedDict):
     question: str
     query: str
     result: str
     answer: str
+
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["  OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+
+# <---------------- Define the SQL query system prompt ---------------->
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+query_prompt_template = hub.pull("langchain-ai/sql-query-system-prompt")
+
+assert len(query_prompt_template.messages) == 1
+from typing_extensions import Annotated
+
+
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+
+def write_query(state: State, db):
+    """Generate SQL query to fetch information."""
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        }
+    )
+    structured_llm = llm.with_structured_output(QueryOutput)
+    result = structured_llm.invoke(prompt)
+    return {"query": result["query"]}
+
+
 
 
 def display_result():
